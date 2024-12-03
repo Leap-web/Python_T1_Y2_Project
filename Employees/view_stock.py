7f17db09a931e6782d3e8eeefeb2ba2aea2bfbfe
socheatatharoth <cheatatharoth@gmail.com>
2024-12-03 17:57:28 +0700

    edit my file

--- Employees/view_stock.py
+++ Employees/view_stock.py
@@ -3,13 +3,23 @@ file_paths = [
     r"C:\Python_T1_Y2_Project\Employees\airpod_user.txt",
     r"C:\Python_T1_Y2_Project\Employees\macbook.txt"
 ]
-for path in file_paths:
+print("Select a file to open:")
+for idx, file_path in enumerate(file_paths, 1):
+    print(f"{idx}. {file_path}")
+while True:
     try:
-        with open(path, "r") as file:
-            print(f"Contents of {path}:")
-            print(file.read())
-            print("-" * 50)
+        file_choice = int(input("\nWhich file you want to open(1 to 3):")) - 1 
+        if 0 <= file_choice < len(file_paths):
+            selected_file = file_paths[file_choice]
+            with open(selected_file, "r") as file:
+                print(f"\nContents of {selected_file}:")
+                print(file.read())
+            break
+        else:
+            print("Invalid selection! Please enter a valid file number.")
+    except ValueError:
+        print("Invalid input! Please enter a number.")
     except FileNotFoundError:
-        print(f"Error: File not found - {path}")
+        print(f"Error: File not found - {selected_file}")
     except Exception as e:
-        print(f"Error: {e} - {path}")
+        print(f"Error: {e}")
