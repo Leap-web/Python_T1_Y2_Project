9fe467d4e8e6f9a98556158560ec4bdba0c8a5b2
Chhunly-Un <unchhunly15@gmail.com>
2024-12-07 17:49:52 +0700

    undate employee_log and file txt

--- employee_log/employee_log.py
+++ employee_log/employee_log.py
@@ -1,17 +1,34 @@
-import getpass
+import msvcrt
 
+def masked_input(prompt= ""):
+    print(prompt, end="", flush=True)
+    password = ""
+    while True:
+        char = msvcrt.getch()
+        if char in {b'\r',b'\n'}:
+            print()
+            break
+        elif char == b'\x80':
+            if len(password) > 0:
+                password = password[:-1]
+                print("\b \b", end='', flush=True)
+        else:
+            password += char.decode()
+            print('*', end='', flush=True)
+    return password
 def check_employee_inf(username, email, ID, password):
     try:
-        with open('inf_employee.txt', 'r') as file:
+        with open(r'C:\Users\User\OneDrive\ドキュメント\GitHub\Python_T1_Y2_Project\employee_log\inf_employee.txt', 'r') as file:
             for line in file:
                 stored_username, stored_email, stored_ID, stored_password = line.strip().split(',')
+                '''print(f"Comparing: {stored_username.strip()} == {username.strip()}?")'''
                 if (stored_username.strip().lower() == username.strip().lower() and
                     stored_email.strip().lower() == email.strip().lower() and
                     stored_ID.strip() == ID.strip() and
                     stored_password.strip() == password.strip()):
                     return True
 
-                return False
+            return False
     except FileNotFoundError:
         print("Employee informatioon not foun!")
         return False
@@ -20,7 +37,7 @@ def employee_login():
     employee_username = input("Enter your username: ")
     employee_email = input("Enter your email: ")
     employee_id = input("Enter your ID: ")
-    employee_password = getpass.getpass("Enter your password: ")
+    employee_password = masked_input("Enter your password: ")
     
     if not check_employee_inf(employee_username,employee_email,employee_id,employee_password):
         print("###############YOU ARE NOT OUR EMPLOYEE YOU ARE HACKER###############")
