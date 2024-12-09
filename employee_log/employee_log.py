253595260709c3367ef24de099b63ae5fa6c8bf5
Chhunly-Un <unchhunly15@gmail.com>
2024-12-10 05:34:12 +0700

    edit

--- employee_log/employee_log.py
+++ employee_log/employee_log.py
@@ -21,7 +21,6 @@ def check_employee_inf(username, email, ID, password):
         with open(r'C:\Users\User\OneDrive\ドキュメント\GitHub\Python_T1_Y2_Project\employee_log\inf_employee.txt', 'r') as file:
             for line in file:
                 stored_username, stored_email, stored_ID, stored_password = line.strip().split(',')
-                
                 if (stored_username.strip().lower() == username.strip().lower() and
                     stored_email.strip().lower() == email.strip().lower() and
                     stored_ID.strip() == ID.strip() and
