5825bbbc2a2ee6a8c0b911da1e271703089b3049
Chhunly-Un <unchhunly15@gmail.com>
2024-12-12 09:03:30 +0700

    update log

--- employee_log/employee_log.py
+++ employee_log/employee_log.py
@@ -22,6 +22,54 @@ def encrypt_password(password):
             return password
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
         return hashed_password
+
+def save_admin_to_file(self, username, id, email, hashed_password):
+    with open(self.employee_file, "a") as file:
+            file.write(f"Username: {username}, ID: {id}, Email: {email}, Password: {hashed_password}\n")
+    print("Employee account created successfully.")
+
+def create_employee_account(self):
+    while True:
+        print("Eg: Name: John_Doe")
+        username = input("Enter employee_account name: ")
+        if username and "_" in username and not any(c.isspace() for c in username) and any(c.isupper() for c in username) and any (c.islower() for c in username) and any(c.islower() for c in username):
+            while True:
+                print("Eg: Email: john.doe@employee.iec.com")
+                email = input("Enter employee_account email: ")
+                if "@employee.iec.com" in email and not any(c.isupper() for c in email) and not any(c.isspace() for c in email):
+                    while True:
+                        id = input("Create employee_account ID (Eg: IDTB1234): ")
+                        if id.startswith("IDTB"):
+                            while True:
+                                passwords = masked_input("Create password: ")
+                                if (
+                                    len(passwords) >= 8
+                                    and any(c.isupper() for c in passwords)
+                                    and any(c.islower() for c in passwords)
+                                    and any(c.isdigit() for c in passwords)
+                                    and any(c in "!@#$%^&*()_+-=" for c in passwords)
+                                    ):
+                                    self.log_action("Added new employee_account", "employee_account")
+                                    hashed_password = self.hash_password(passwords)
+
+                                        # Save_account details to file         
+                                    self.save__to_file(username, id, email, hashed_password)
+                                    return
+                                else:
+                                    print("Password must contain at least 8 characters, including uppercase, lowercase, a number, and a special character.")
+                        else:
+                                print("Employee_account ID must start with 'IDTB'")
+                else:
+                        print("Invalid email format. Ensure it ends with '@employee_account.iec.com' and contains no spaces or uppercase letters.")
+        else:
+                print("Invalid name format. Ensure it contains an underscore (_) and no spaces.")
+
+        hashed_pw = self.hash_password()
+        self.save_employee_to_file(username, email, id, hashed_pw)
+        self.employee.append({"username": username, "email": email, "id": id, "password": hashed_pw})
+        print("Employee account create successful!\n")
+        break
+
 def check_employee_inf(username, email, ID, password):
     hashed_password = encrypt_password(password)
     try:
